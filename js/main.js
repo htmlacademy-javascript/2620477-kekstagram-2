0a78a42105433e7a172f75ba0a64d74b4431917d
Dmitrii Nikoshin <d.nikoshin@gmail.com>
2025-06-15 14:14:02 +0300

    ex12.1 added filters, removed functions.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ import { initPhotoModal } from './photo-modal.js';
 import { initUploadForm } from './upload-form.js';
 import { initValidation } from './validation.js';
 import { showDataErrorMessage } from './utils.js';
+import { initFilters } from './filters.js';
 
 getData()
   .then((photos) => {
     renderThumbnails(photos);
     initPhotoModal(photos);
+    initFilters(photos);
   })
   .catch(() => {
     showDataErrorMessage();
